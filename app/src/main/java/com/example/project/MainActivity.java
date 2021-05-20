8b727020321a8873fe785b263873356d8f8e31fd
a17jespe <a17jespe@student.his.se>
2021-05-21 00:58:45 +0200

    Reused previously developed implementation from assignment 'Networking' Adapted implementation for new purpose Changed variable names Changed variable types

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.project;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,19 +9,65 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<byggnadsobjekt> objektArrayList = new ArrayList<>();
+    private ArrayAdapter<byggnadsobjekt> objektAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        objektArrayList = new ArrayList<>();
+        objektAdapter = new ArrayAdapter<>(this, R.layout.layoutlistobjekts,R.id.listtextView , objektArrayList);
+
+        ListView thelistview = (ListView) findViewById(R.id.my_listview);
+        thelistview.setAdapter(objektAdapter);
+
+        thelistview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                String name = objektArrayList.get(position).getName();
+                Integer size = objektArrayList.get(position).getSize();
+                String location = objektArrayList.get(position).getLocation();
+                Integer cost = objektArrayList.get(position).getCost();
+                String company = objektArrayList.get(position).getCompany();
+                String message = "The object " +  name + " has a size of " + size + ", a cost of " + cost + ", is located at"+ location +", and is owned by "+ company;
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=a17jespe");
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +77,73 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+    }
+
+
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            Log.e("QUERY ANSWER: ", json);
+            try {
+                objektAdapter.clear();
+                JSONArray jsonArray = new JSONArray(json);
+                for (int i=0; i < jsonArray.length(); i++) // Goes through i in array based on array length.
+                {
+                    JSONObject oneObject = jsonArray.getJSONObject(i); //Assigns object to variable based on current object in the array jsonArray.
+                    String name = oneObject.getString("name");
+                    String location = oneObject.getString("location");
+                    Integer size= oneObject.getInt("size");
+                    String company = oneObject.getString("company");
+                    Integer cost = oneObject.getInt("cost");
+                    objektArrayList.add(new byggnadsobjekt(name, size, location, company, cost)); //Adds a new mountain in arraylist with name, location and height (int) values.
+                }
+
+                objektAdapter.notifyDataSetChanged();
+
+            } catch (JSONException e) {
+                Log.e("JSON Stuff: ",e.getMessage());
+            }
+        }
     }
 
     @Override
@@ -41,16 +155,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
+        if (id == R.id.about) {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/project/byggnadsobjekt.java
@@ -0,0 +1,32 @@
+package com.example.project;
+
+public class byggnadsobjekt {
+
+    private String name;
+    private int size;
+    private String location;
+    private String company;
+    private Integer cost;
+
+    public byggnadsobjekt(String name, int size, String location, String company, Integer cost) {
+        this.name = name;
+        this.size = size;
+        this.location = location;
+        this.company = company;
+        this.cost = cost;
+    }
+
+
+    @Override
+    public String toString(){
+        return name;
+    }
+
+    public String getCompany() {return this.company;}
+    public String getName() {return this.name;}
+    public int getSize() {return this.size;}
+    public String getLocation() {return this.location;}
+    public Integer getCost() {return this.cost;}
+
+
+}
