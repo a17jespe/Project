445757efe45a5c867a42b4c28c1de853d69a6e06
a17jespe <a17jespe@student.his.se>
2021-05-21 03:46:25 +0200

    An attempt to use Recycler was made, and abandoned.

--- app/src/main/java/com/example/project/FirstFragment.java
+++ app/src/main/java/com/example/project/FirstFragment.java
@@ -1,121 +1,36 @@
 package com.example.project;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 import com.example.project.dummy.DummyContent;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
 
 /**
  * A fragment representing a list of Items.
  */
-
 public class FirstFragment extends Fragment {
 
-    private Integer mColumnCount;
-    private ArrayList<byggnadsobjekt> objektArrayList = new ArrayList<>();
-    private ArrayAdapter<byggnadsobjekt> objektAdapter;
-
-    private static final String ARG_COLUMN_COUNT = "column-count";
-
-    private class JsonTask extends AsyncTask<String, String, String> {
-
-        private HttpURLConnection connection = null;
-        private BufferedReader reader = null;
-
-        protected String doInBackground(String... params) {
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-
-                StringBuilder builder = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null && !isCancelled()) {
-                    builder.append(line).append("\n");
-                }
-                return builder.toString();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String json) {
-            Log.e("QUERY ANSWER: ", json);
-            try {
-                objektAdapter.clear();
-                JSONArray jsonArray = new JSONArray(json);
-                mColumnCount = jsonArray.length();
-                for (int i=0; i < jsonArray.length(); i++) // Goes through i in array based on array length.
-                {
-                    JSONObject oneObject = jsonArray.getJSONObject(i); //Assigns object to variable based on current object in the array jsonArray.
-                    String name = oneObject.getString("name");
-                    String location = oneObject.getString("location");
-                    Integer size= oneObject.getInt("size");
-                    String company = oneObject.getString("company");
-                    Integer cost = oneObject.getInt("cost");
-                    objektArrayList.add(new byggnadsobjekt(name, size, location, company, cost)); //Adds a new mountain in arraylist with name, location and height (int) values.
-                }
-
-                objektAdapter.notifyDataSetChanged();
-
-            } catch (JSONException e) {
-                Log.e("JSON Stuff: ",e.getMessage());
-            }
-        }
-    }
-
     // TODO: Customize parameter argument names
-
+    private static final String ARG_COLUMN_COUNT = "column-count";
     // TODO: Customize parameters
-    // private int mColumnCount=5;
+    private int mColumnCount = 1;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
+    public FirstFragment() {
+    }
 
     // TODO: Customize parameter initialization
     @SuppressWarnings("unused")
@@ -136,30 +51,29 @@ public class FirstFragment extends Fragment {
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_first_list, container, false);
-
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_first_list, container, false);
 //        view.findViewById(R.id.button_newobject).setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
-//                NavHostFragment.findNavController(fragment_first.this)
+//                NavHostFragment.findNavController(FirstFragment.this)
 //                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
 //            }
 //        });
-
-        // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS));
-        }
-        return view;
-    }
+//
+//        // Set the adapter
+//        if (view instanceof RecyclerView) {
+//            Context context = view.getContext();
+//            RecyclerView recyclerView = (RecyclerView) view;
+//            if (mColumnCount <= 1) {
+//                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+//            } else {
+//                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+//            }
+//            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(DummyContent.ITEMS));
+//        }
+//        return view;
+//    }
 }
--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -8,9 +8,11 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.util.Log;
 import android.view.View;
+
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -36,8 +38,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<byggnadsobjekt> objektArrayList = new ArrayList<>();
-    private ArrayAdapter<byggnadsobjekt> objektAdapter;
+
+
+//    private ArrayList<byggnadsobjekt> objektArrayList = new ArrayList<>();
+//    private ArrayAdapter<byggnadsobjekt> objektAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,104 +53,108 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        objektArrayList = new ArrayList<>();
-        objektAdapter = new ArrayAdapter<>(this, R.layout.layoutlistobjekts,R.id.listtextView , objektArrayList);
-
-        ListView thelistview = findViewById(R.id.my_listview);
-        thelistview.setAdapter(objektAdapter);
-
-        thelistview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-                String name = objektArrayList.get(position).getName();
-                Integer size = objektArrayList.get(position).getSize();
-                String location = objektArrayList.get(position).getLocation();
-                Integer cost = objektArrayList.get(position).getCost();
-                String company = objektArrayList.get(position).getCompany();
-                String message = "The object " +  name + " has a size of " + size + ", a cost of " + cost + ", is located at"+ location +", and is owned by "+ company;
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
-            }
-        });
+//        objektArrayList = new ArrayList<>();
+//        objektAdapter = new ArrayAdapter<>(this, R.layout.layoutlistobjekts,R.id.listtextView , objektArrayList);
+//
+//        ListView thelistview = findViewById(R.id.my_listview);
+//        thelistview.setAdapter(objektAdapter);
+//
+//        thelistview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+//                String name = objektArrayList.get(position).getName();
+//                Integer size = objektArrayList.get(position).getSize();
+//                String location = objektArrayList.get(position).getLocation();
+//                Integer cost = objektArrayList.get(position).getCost();
+//                String company = objektArrayList.get(position).getCompany();
+//                String message = "The object " +  name + " has a size of " + size + ", a cost of " + cost + ", is located at"+ location +", and is owned by "+ company;
+//                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
     }
 
 
-    private class JsonTask extends AsyncTask<String, String, String> {
+//    private class JsonTask extends AsyncTask<String, String, String> {
+//
+//        private HttpURLConnection connection = null;
+//        private BufferedReader reader = null;
+//
+//        protected String doInBackground(String... params) {
+//            try {
+//                URL url = new URL(params[0]);
+//                connection = (HttpURLConnection) url.openConnection();
+//                connection.connect();
+//
+//                InputStream stream = connection.getInputStream();
+//                reader = new BufferedReader(new InputStreamReader(stream));
+//
+//                StringBuilder builder = new StringBuilder();
+//                String line;
+//                while ((line = reader.readLine()) != null && !isCancelled()) {
+//                    builder.append(line).append("\n");
+//                }
+//                return builder.toString();
+//            } catch (MalformedURLException e) {
+//                e.printStackTrace();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                if (connection != null) {
+//                    connection.disconnect();
+//                }
+//                try {
+//                    if (reader != null) {
+//                        reader.close();
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String json) {
+//            Log.e("QUERY ANSWER: ", json);
+//            try {
+//                objektAdapter.clear();
+//                JSONArray jsonArray = new JSONArray(json);
+//                for (int i=0; i < jsonArray.length(); i++) // Goes through i in array based on array length.
+//                {
+//                    JSONObject oneObject = jsonArray.getJSONObject(i); //Assigns object to variable based on current object in the array jsonArray.
+//                    String name = oneObject.getString("name");
+//                    String location = oneObject.getString("location");
+//                    Integer size= oneObject.getInt("size");
+//                    String company = oneObject.getString("company");
+//                    Integer cost = oneObject.getInt("cost");
+//                    objektArrayList.add(new byggnadsobjekt(name, size, location, company, cost)); //Adds a new mountain in arraylist with name, location and height (int) values.
+//                }
+//
+//                objektAdapter.notifyDataSetChanged();
+//
+//            } catch (JSONException e) {
+//                Log.e("JSON Stuff: ",e.getMessage());
+//            }
+//        }
+//    }
 
-        private HttpURLConnection connection = null;
-        private BufferedReader reader = null;
-
-        protected String doInBackground(String... params) {
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-                InputStream stream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
-
-                StringBuilder builder = new StringBuilder();
-                String line;
-                while ((line = reader.readLine()) != null && !isCancelled()) {
-                    builder.append(line).append("\n");
-                }
-                return builder.toString();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String json) {
-            Log.e("QUERY ANSWER: ", json);
-            try {
-                objektAdapter.clear();
-                JSONArray jsonArray = new JSONArray(json);
-                for (int i=0; i < jsonArray.length(); i++) // Goes through i in array based on array length.
-                {
-                    JSONObject oneObject = jsonArray.getJSONObject(i); //Assigns object to variable based on current object in the array jsonArray.
-                    String name = oneObject.getString("name");
-                    String location = oneObject.getString("location");
-                    Integer size= oneObject.getInt("size");
-                    String company = oneObject.getString("company");
-                    Integer cost = oneObject.getInt("cost");
-                    objektArrayList.add(new byggnadsobjekt(name, size, location, company, cost)); //Adds a new mountain in arraylist with name, location and height (int) values.
-                }
-
-                objektAdapter.notifyDataSetChanged();
-
-            } catch (JSONException e) {
-                Log.e("JSON Stuff: ",e.getMessage());
-            }
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.button_displayobject) {
-            new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=a17jespe");
+            // new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=a17jespe");
+            return true;
+
         }
         if (id == R.id.button_about) {
             return true;
@@ -156,4 +165,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
